675ee95f3348f79d4b9cb882100ee9332ee840ea
Emmanuel Dalyop <108156886+EcodeJR@users.noreply.github.com>
2023-02-16 13:02:43 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -13,15 +13,45 @@ function CoinGecko() {
                                     <img src="${coin.item.small}" alt="" id="img1">
                                 </div>
                                 <div class="dets">
-                                    <h1 id="name">Name:${coin.item.name}</h1>
-                                    <h2 id="symbol">Symbol:${coin.item.symbol}</h2>
-                                    <h1 id="name">Price in BTC:${coin.item.price_btc}</h1>
+                                    <h1 class="name">Name: ${coin.item.name}</h1>
+                                    <h2 class="symbol">Symbol: ${coin.item.symbol}</h2>
+                                    <h1 class="name">Price in BTC: ${coin.item.price_btc}</h1>
                                 </div>
                             </div>
                         `);
+
             });
         }
     });
 };
 
 CoinGecko();
+
+function NFT() {
+    let link = "https://api.coingecko.com/api/v3/nfts/list";
+
+    $.ajax({
+        method: 'get',
+        url: link,
+
+        success: function(data) {
+            data.forEach(item => {
+                $("#container2").append(`
+                            <div class="card">
+                                <div class="image">
+                                    <p id="img2">${item.symbol}</p>
+                                </div>
+                                <div class="dets">
+                                <h1>ID: ${item.id}</h1>
+                                    <h1 class="name">Name: ${item.name}</h1>
+                                    <h2 class="symbol">contract_address: ${item.contract_address}</h2>
+                                    <h1 class="name">platform_id: ${item.asset_platform_id}</h1>
+                                </div>
+                            </div>
+                        `);
+
+            });
+        }
+    });
+};
+NFT();
